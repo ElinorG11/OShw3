4b31c3f223a15c1462d4b5990ec8547a320df08d
ElinorG11 <elinor.g12@gmail.com>
2021-05-31 17:44:59 +0000

    Part 1 + Part 2: v 2.0

---
+++ cmake-build-debug/CMakeFiles/OShw3.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/student/CLionProjects/OShw3/client.c" "/home/student/CLionProjects/OShw3/cmake-build-debug/CMakeFiles/OShw3.dir/client.c.o"
+  "/home/student/CLionProjects/OShw3/output.c" "/home/student/CLionProjects/OShw3/cmake-build-debug/CMakeFiles/OShw3.dir/output.c.o"
+  "/home/student/CLionProjects/OShw3/queue.c" "/home/student/CLionProjects/OShw3/cmake-build-debug/CMakeFiles/OShw3.dir/queue.c.o"
+  "/home/student/CLionProjects/OShw3/request.c" "/home/student/CLionProjects/OShw3/cmake-build-debug/CMakeFiles/OShw3.dir/request.c.o"
+  "/home/student/CLionProjects/OShw3/segel.c" "/home/student/CLionProjects/OShw3/cmake-build-debug/CMakeFiles/OShw3.dir/segel.c.o"
+  "/home/student/CLionProjects/OShw3/server.c" "/home/student/CLionProjects/OShw3/cmake-build-debug/CMakeFiles/OShw3.dir/server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OShw3.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OShw3.dir/client.c.o"
+  "CMakeFiles/OShw3.dir/output.c.o"
+  "CMakeFiles/OShw3.dir/queue.c.o"
+  "CMakeFiles/OShw3.dir/request.c.o"
+  "CMakeFiles/OShw3.dir/segel.c.o"
+  "CMakeFiles/OShw3.dir/server.c.o"
+  "OShw3"
+  "OShw3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OShw3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
